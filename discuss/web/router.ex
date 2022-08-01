db798446694e0d8b5fd3da5bcf1f17024793495d
Chris Reed <chrisreed87@gmail.com>
2022-08-01 16:16:08 -0700

    finish section 10

--- discuss/web/router.ex
+++ discuss/web/router.ex
@@ -15,11 +15,12 @@ defmodule Discuss.Router do
 
   scope "/", Discuss do
     pipe_through :browser # Use the default browser stack
-
-    get "/", TopicController, :index
-    get "/topics/new", TopicController, :new
-    post "/topics", TopicController, :create
-    get "/topics/:id/edit", TopicController, :edit
+    resources "/", TopicController
+#    get "/", TopicController, :index
+#    get "/topics/new", TopicController, :new
+#    post "/topics", TopicController, :create
+#    get "/topics/:id/edit", TopicController, :edit
+#    put "/topics/:id", TopicController, :updat
   end
 
   # Other scopes may use custom stacks.
